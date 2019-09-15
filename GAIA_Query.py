0467863a8783e5ef75c2fa3c1a4869ff0b178fac
TFT-Ben <ben.cartwright@theflametrench.com>
2019-09-15 18:41:09 +0100

    initial query for image centred data finalised

---
+++ GAIA_Query.py
@@ -0,0 +1,51 @@
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+from astroquery.gaia import Gaia
+
+import warnings
+warnings.filterwarnings("ignore", module='astropy.io.votable.tree')
+
+mag_ub = 20.0                                       # magnitude upper bound for query
+
+# how to import platesolved coords from another python file? Do everything in same script?
+# degrees?
+# TODO use SkyCoord
+image_ctr_ra     = 200.0
+image_ctr_dec    = -60.0
+
+# image fov calculations - compare against platesolved data?
+# image extents expressed as image size in a given axis in arcseconds
+image_y_extent  = 10
+image_x_extent  = 15
+
+# query stringbuilding
+
+
+# Gaia final query - 
+# SELECT ra, dec, phot_g_mean_mag, r_est, teff_val, designation \
+# FROM external.gaiadr2_geometric_distance \
+# JOIN gaiadr2.gaia_source USING (source_id) \
+# WHERE CONTAINS (POINT('ICRS', ra, dec), BOX('ICRS', image_ctr_ra, image_ctr_dec, image_x_extent * 3600, image_y_extent * 3600)) = 1 \
+# AND phot_g_mean_mag < mag_ub
+# AND teff_val > 0
+
+# Gaia test query - SELECT TOP 1000000000 (ra/360*24) AS RA, dec, r_est, teff_val, \
+# phot_g_mean_mag, designation \
+# FROM external.gaiadr2_geometric_distance \
+# JOIN gaiadr2.gaia_source \
+# USING (source_id) \
+# WHERE phot_g_mean_mag > 16.95 AND phot_g_mean_mag < 17 AND teff_val > 0
+
+# launch async job, file dump as .csv
+
+# ------------------------------------------------------------------------------------------
+
+# Gaia table headings:
+# solution_id     - record of subsystem version for data generation/input data
+# designation     - source designation derived from source_id, unique across data releases
+# source_id       - id encoding rough position and other info, unique in a particular DR
+# ra              - right ascension, icrs, degrees
+# dec             - declination, ircs, degrees
+# phot_g_mean_mag - mean magnitude in G band, float
+# teff_val        - stellar effective temperature, float, K
+# r_est           - estimated distance, parsec, from extenral.gaiadr2_geometric_distance
